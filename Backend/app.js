a008a468ee5e0aee4bf1ae75c216d4428077f14f
Vishal <vishalkalawad51@gmail.com>
2026-05-22 02:08:15 +0530

    Deploying App

--- Backend/app.js
+++ Backend/app.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-url.vercel.app"
+      "https://crowdfunding-system.vercel.app/"
     ],
     credentials: true,
   })
